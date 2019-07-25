3cf1c789d4fabf6210d37c030e1d57a8e73bc71f
Joe Pritchett <joempritchett@gmail.com>
2019-07-24 20:38:43 -0700

    create main app class, grab screen dimensions, render empty GLView on mobile

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { GLView } from 'expo-gl';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      screenHeight: null,
+      screenWidth: null,
+    }
+  }
+
+  componentWillMount(){
+    const {height, width} = Dimensions.get('window');
+    this.setState({screenHeight: height, screenWidth: width})
+  }
+
+  _onContextCreate = async gl => {
+
+  }
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <Text style={{color:'red'}}>Open up App.js to start working on your app!</Text>
+        <GLView style={[styles.glview, {width: this.state.screenWidth, height: this.state.screenWidth}]} onContextCreate={this._onContextCreate}/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgrey',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  glview: {
+    borderColor: 'red',
+    borderRadius: 1,
   },
 });
