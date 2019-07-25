d3b07a924a6e028765398510845ef8250a1b4f07
Joe Pritchett <joempritchett@gmail.com>
2019-07-24 21:27:04 -0700

    add boilerplate code to render a triangle on mobile. web not currently functioning

--- App.js
+++ App.js
@@ -16,8 +16,103 @@ export default class App extends React.Component {
     this.setState({screenHeight: height, screenWidth: width})
   }
 
-  _onContextCreate = async gl => {
+  _onContextCreate = gl => {
 
+    const vertexShaderSource =
+    `
+    #version 300 es
+    in vec4 a_position;
+    void main () {
+      gl_Position = a_position;
+    }
+    `;
+
+    const fragmentShaderSource =
+    `
+    #version 300 es
+    precision mediump float;
+    out vec4 outColor;
+    void main () {
+      outColor = vec4(1, .5, 0, 1);
+    }
+    `
+
+    function createShader(gl, type, source) {
+      const shader = gl.createShader(type);
+      gl.shaderSource(shader, source);
+      gl.compileShader(shader);
+      const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+      if (success) {
+        return shader;
+      }
+
+      console.log('shader failed to compile');
+      console.log(gl.getShaderInfoLog(shader));
+      gl.deleteShader(shader);
+    }
+
+    const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+
+    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    function createProgram(gl, vertexShader, fragmentShader) {
+      const program = gl.createProgram();
+      gl.attachShader(program, vertexShader);
+      gl.attachShader(program, fragmentShader);
+      gl.linkProgram(program);
+      const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+      if (success) {
+        return program;
+      }
+
+      console.log('program failed to link');
+      console.log(gl.getProgramInfoLog(program));
+      gl.deleteProgram(program);
+    }
+
+    const program = createProgram(gl, vertexShader, fragmentShader);
+
+    const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+
+    const vao = gl.createVertexArray();
+    gl.bindVertexArray(vao);
+    gl.enableVertexAttribArray(positionAttributeLocation);
+
+    const size = 2;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    const primitiveType = gl.TRIANGLES;
+    const count = 3;
+    gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset)
+
+
+    const onTick = () => {
+      const positions = [
+        0, 0,
+        0, 0.5,
+        0.7, 0,
+      ];
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+      gl.clearColor(.2, 0, .6, 1);
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+      gl.useProgram(program);
+      gl.drawArrays(primitiveType, offset, count);
+      gl.endFrameEXP();
+    };
+
+    const animate = () => {
+      if (gl) {
+        this._rafID = requestAnimationFrame(animate);
+        onTick(gl);
+      }
+    };
+    animate();
   }
 
   render() {
