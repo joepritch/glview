b74e861a48d144fcd458cab4cf18cc6d7865a602
Joe Pritchett <joempritchett@gmail.com>
2019-07-29 18:18:50 -0700

    re-fix bug that prevented webgl from rendering more than two triangles (original fix turned out to be a misguided workaround), add translation

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ export default class App extends React.Component {
       screenWidth: null,
       xValue: 0,
       yValue: 0,
+      xValueDelta: 0,
+      yValueDelta: 0,
       width: 100,
       height: 100,
       count: 0,
@@ -24,91 +26,59 @@ export default class App extends React.Component {
     this.PanResponder = PanResponder.create({
       onStartShouldSetPanResponder: (event, gestureState) => true,
       onPanResponderMove: (event, gestureState) => {
-        this.setState({xValue: (gestureState.dx), yValue: (gestureState.dy)})
+        this.setState({xValue: (gestureState.dx + this.state.xValueDelta), yValue: (gestureState.dy + this.state.yValueDelta)})
+      },
+      onPanResponderRelease: (event, gestureState) => {
+        this.setState({xValueDelta: (gestureState.dx + this.state.xValueDelta), yValueDelta: (gestureState.dy + this.state.yValueDelta)})
       }
     })
   }
 
+  createRandomTriangle = (gl) => {
+    return this.createTriangle(gl, Math.random() * 200, Math.random() * 200, Math.random() * 200, Math.random() * 200)
+  }
+
+  createTriangle = (gl, width, height, xValue, yValue) => {
+    //generate a triangle with specified size and a_position
+    const triangleVerticies = [
+      xValue, yValue,
+      xValue + width, yValue,
+      xValue, yValue + height,
+    ];
+
+
+    return triangleVerticies;
+  }
+
+  createRectangle = (gl, width, height, xValue, yValue) => {
+    //generate a rectangle with specified size and position
+    const firstTriangle = this.createTriangle(gl, width, height, xValue, yValue);
+
+    const secondTriangle = this.createTriangle(gl, (width * -1), (height * -1), (xValue + width), (yValue + height));
+
+    const rectangle = firstTriangle.concat(secondTriangle);
+
+    return rectangle;
+  }
+
+  createF = (gl, width, height, xValue, yValue) => {
+    //generate rectangles in the shape of an 'F' with a specified height and position
+    const thickness = width / 4;
+
+    const column = this.createRectangle(gl, thickness, height, xValue, yValue);
+
+    const topRow = this.createRectangle(gl, (width - thickness), thickness, (xValue + thickness), yValue);
+
+    const middleRow = this.createRectangle(gl, (width - (thickness * 2)), thickness, (xValue + thickness), (yValue + (thickness * 2)));
+
+    const f = column.concat(topRow, middleRow);
+
+    return f;
+  }
 
   _onContextCreate = async (gl) => {
 
     //functions
-    const createRandomTriangle = () => {
-      return createTriangle(Math.random() * 200, Math.random() * 200, Math.random() * 200, Math.random() * 200, )
-    }
-
-    const createTriangle = (width, height, xValue, yValue) => {
-      //generate a triangle with specified size and a_position
-      const triangleVerticies = [
-        xValue, yValue,
-        xValue + width, yValue,
-        xValue, yValue + height,
-      ];
-
-      gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array(triangleVerticies),
-        gl.STATIC_DRAW
-      );
-
-      const primitiveType = gl.TRIANGLES;
-      const offset = 0;
-      const count = 3;
-
-      gl.drawArrays(primitiveType, offset, count);
-    }
-
-    const createRectangle = (width, height, xValue, yValue) => {
-      //generate a rectangle with specified size and position
-      const firstTriangle = createTriangle(width, height, xValue, yValue);
-
-      const secondTriangle = createTriangle((width * -1), (height * -1), (xValue + width), (yValue + height));
-
-      // const rectangle = firstTriangle.concat(secondTriangle)
-      //
-      // return rectangle;
-    }
-
-    const createF = (width, height, xValue, yValue) => {
-      //generate rectangles in the shape of an 'F' with a specified height and position
-      const thickness = width / 4;
-
-      const column = createRectangle(thickness, height, xValue, yValue);
-
-      const topRow = createRectangle((width - thickness), thickness, (xValue + thickness), yValue);
-
-      const middleRow = createRectangle((width - (thickness * 2)), thickness, (xValue + thickness), (yValue + (thickness * 2)));
-
-      // // console.log(column);
-      // const finalArray = column.concat(topRow, middleRow)
-      // // console.log(finalArray);
-      //
-      // return finalArray;
-    }
-
-    const getColors = () => {
-      // pick 2 random colors
-      const r1 = Math.random();
-      const g1 = Math.random();
-      const b1 = Math.random();
-
-      const r2 = Math.random();
-      const g2 = Math.random();
-      const b2 = Math.random();
-
-      const colorArray =
-      [
-        r1, g1, b1, 1,
-        r1, g1, b1, 1,
-        r2, g2, b2, 1,
-        r2, g2, b2, 1,
-        r2, g2, b2, 1,
-        r1, g1, b1, 1
-      ];
-
-      return colorArray;
-    }
-
     const createShader = (gl, type, source) => {
       //create and compile shaders
       const shader = gl.createShader(type);
@@ -140,28 +110,7 @@ export default class App extends React.Component {
       gl.deleteProgram(program);
     }
 
-    const setColor = (gl, colorArray) => {
-      const colorBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-
-      const colorAttributeLocation = gl.getAttribLocation(program, "a_color");
-      gl.enableVertexAttribArray(colorAttributeLocation);
-
-      const size = 4;
-      const type = gl.FLOAT;
-      const normalize = false;
-      const stride = 0;
-      const offset = 0;
-      gl.vertexAttribPointer(colorAttributeLocation, size, type, normalize, stride, offset)
-
-      gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array(colorArray),
-        gl.STATIC_DRAW
-      );
-    }
-
-    const setGeometry = (gl) => {
+    const setGeometry = (gl, data) => {
       const vertexBuffer = gl.createBuffer();
       gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
@@ -174,21 +123,29 @@ export default class App extends React.Component {
       const stride = 0;
       const offset = 0;
       gl.vertexAttribPointer(vertexAttributeLocation, size, type, normalize, stride, offset)
+
+      gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(data),
+        gl.STATIC_DRAW
+      );
+
     }
 
-    //shaders
+    //shader sources
     const vertexShaderSource =
       `
       attribute vec2 a_position;
+      uniform vec2 u_translation;
       uniform vec2 u_resolution;
-      attribute vec4 a_color;
       varying vec4 v_color;
       void main () {
-        vec2 zeroToOne = a_position / u_resolution;
+        vec2 position = a_position + u_translation;
+        vec2 zeroToOne = position / u_resolution;
         vec2 zeroToTwo = zeroToOne * 2.0;
         vec2 clipSpace = zeroToTwo - 1.0;
         gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
-        v_color = a_color;
+        v_color = gl_Position * 0.5 + 0.5;
       }
       `;
 
@@ -201,6 +158,7 @@ export default class App extends React.Component {
       }
       `;
 
+    //shaders
     const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
 
     const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
@@ -210,17 +168,27 @@ export default class App extends React.Component {
     gl.useProgram(program);
 
     //main
-    setColor(gl, getColors());
-
-    setGeometry(gl);
-
     const resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
     gl.uniform2f(resolutionUniformLocation, 400, 400);
+
+    const translationUniformLocation = gl.getUniformLocation(program, "u_translation");
+
+
+    const renderObject = this.createF(gl, this.state.width, this.state.height, 25, 25).concat(this.createTriangle(gl, this.state.width, this.state.height, 25, 25))
+
+    setGeometry(gl, renderObject);
 
     const onTick = () => {
       gl.clearColor(1, 1, 1, 1);
       gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-      createF(this.state.width, this.state.height, this.state.xValue, this.state.yValue);
+      const translation = [this.state.xValue, this.state.yValue];
+      gl.uniform2fv(translationUniformLocation, translation);
+
+      const primitiveType = gl.TRIANGLES;
+      const offset = 0;
+      const count = 21;
+
+      gl.drawArrays(primitiveType, offset, count);
       gl.endFrameEXP();
     };
 
@@ -246,6 +214,8 @@ export default class App extends React.Component {
     );
   }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
--- notes.md
+++ notes.md
@@ -1,3 +1,19 @@
 current issues:
-webgl can't render more than two triangles
-renders aren't moving when using the panresponder
+
+code might need to be restructured
+
+need to add translation (right now translation is being handled by pan handler and state)
+
+need to add rotation
+
+need to add scale
+
+need to add matrices
+
+need to add cross platform controls
+
+need to learn how to render points and lines
+
+final goal is to supply a list of data and generate a graph based on said data
+
+graphs include point and line, bar, pie, 3d planes, 3d bar, 3d pie, 
